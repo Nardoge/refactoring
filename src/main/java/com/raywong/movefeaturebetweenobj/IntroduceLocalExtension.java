d8db6bf84767c1494f12e75ad81561dd36302e93
Ray.Wong <nardoge@163.com>
2020-06-18 16:20:58 +0800

    Update IntroduceLocalExtension.java
    补充注释

--- src/main/java/com/raywong/movefeaturebetweenobj/IntroduceLocalExtension.java
+++ src/main/java/com/raywong/movefeaturebetweenobj/IntroduceLocalExtension.java
@@ -3,11 +3,15 @@ package com.raywong.movefeaturebetweenobj;
 import java.util.Date;
 
 /**
- * @Description: TODO
+ * @Description: 引入本地扩展
  * @Author Ray.Wong
  * @Date 2020/6/17 20:18
  */
 public class IntroduceLocalExtension {
+
+    /**
+     * 使用子类进行扩展 或者也可以使用包装类
+     */
 
     class MfDateSub extends Date{
 
